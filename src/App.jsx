11a39280a99890c74605d7f81243cee86520b248
Hamza <144331253+hamzakh86@users.noreply.github.com>
2025-07-14 16:36:30 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -643,7 +643,7 @@ const App = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1.5 }}
-          className="absolute bottom-8 left-1/2 transform -translate-x-1/2"
+          className="absolute bottom-0 left-1/2 transform -translate-x-1/2"
         >
           <motion.div
             animate={{ y: [0, 10, 0] }}
