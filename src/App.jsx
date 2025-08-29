84439c145b51c8a4009b2a8e7c3733a9ba312d81
Hamza <144331253+hamzakh86@users.noreply.github.com>
2025-08-29 17:40:04 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -672,7 +672,7 @@ const technologies = [
                     className="w-full sm:w-auto"
                   >
                     <Download className="mr-2 h-4 w-4" />
-                    View CV
+                    Resume
                     <ChevronDown className="ml-2 h-4 w-4" />
                   </Button>
                   <AnimatePresence>
